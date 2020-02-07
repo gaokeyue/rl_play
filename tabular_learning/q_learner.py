a69db6425916cf66cc6d1740a25f4932baba100d
yiru <yiru.wang1995@aliyun.com>
2020-02-07 09:47:35 +0800

    fix bugs

--- tabular_learning/q_learner.py
+++ tabular_learning/q_learner.py
@@ -13,44 +13,41 @@ class QLearner(Agent):
         self.alpha = alpha
         self.gamma = gamma
 
-    def choose_action(self, state, q, random = True):
+    def choose_action(self, state, q, random=True):
         if np.random.binomial(1, self.epsilon) == 1 & random:
             chosen_action = np.random.choice(self.game.available_actions(state))
         else:
             chosen_action = np.random.choice(self.greedy_pick(q[state], find_one=False))
         return chosen_action
 
-    def q(self, episodes = 50*10**5):
+    def q(self, episodes=50 * 10 ** 5):
         game = self.game
         q = game.q_initializer()
         for i in tqdm(range(episodes)):
             state = game.reset()
             is_terminal = False
-            reward = 0
             while not is_terminal:
-                # state = game._state
                 action = self.choose_action(state, q)
                 next_state, reward, is_terminal = game.one_move(action)
                 target = np.max(list(value.average for key, value in q[next_state].items()))
-                q[state][action].average += self.alpha * (reward +\
-                                self.gamma * target - q[state][action].average)
+                q[state][action].average += self.alpha * (reward + \
+                                                          self.gamma * target - q[state][action].average)
                 state = next_state
         return q
 
-
-    def sarsa(self, episodes = 50*10**5):
+    def sarsa(self, episodes=50 * 10 ** 5):
         game = self.game
         q = game.q_initializer()
         for i in tqdm(range(episodes)):
             state = game.reset()
             is_terminal = False
-            reward = 0
             action = self.choose_action(state, q)
             while not is_terminal:
                 next_state, reward, is_terminal = game.one_move(action)
                 next_action = self.choose_action(next_state, q)
-                q[state][action].average += self.alpha*(reward + self.gamma
-                                    * q[next_state][next_action].average-q[state][action].average)
+                q[state][action].average += self.alpha * (reward + self.gamma
+                                                          * q[next_state][next_action].average - q[state][
+                                                              action].average)
                 state = next_state
                 action = next_action
         return q
@@ -66,20 +63,22 @@ class QLearner(Agent):
             while not is_terminal:
                 next_state, reward, is_terminal = game.one_move(action)
                 next_action = self.choose_action(next_state, q)
-                candidate_lst = list(q[next_state].values()) + [q[next_state][next_action].average]
-                prob_dist = len(q[next_state]) * [self.epsilon]+\
-                            [(len(q[snext_state]) * (1 - self.epsilon))]
-                prob_list = [prob/sum(prob_dist) for prob in prob_dist]
+                candidate_lst = list(v.average for v in q[next_state].values()) + [q[next_state][next_action].average]
+                prob_dist = len(q[next_state]) * [self.epsilon] + \
+                            [(len(q[next_state]) * (1 - self.epsilon))]
+                prob_list = [prob / sum(prob_dist) for prob in prob_dist]
                 target = np.dot(candidate_lst, prob_list)
-                q[state][action].average += self.alpha*(reward + self.gamma
-                                    * target - q[state][action].average)
+                q[state][action].average += self.alpha * (reward + self.gamma
+                                                          * target - q[state][action].average)
                 state = next_state
                 action = next_action
         return q
+
 
 if __name__ == '__main__':
     from game.blackjack import BlackJack
 
     test = BlackJack()
-    test_q = QLearner(test, 0.1, 0.1, 1)
-    q1 = test_q.sarsa(3*10**5)
+    test_q = QLearner(test, 0.05, 0.1, 1)
+    q1 = test_q.expected_sarsa(5 * 10 ** 5)
+    
